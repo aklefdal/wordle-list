e6dfc789d1f79229aa503af8455c095d6c9d9d4d
Alf Kåre Lefdal <alf.kare.lefdal@dnv.com>
2022-01-28 10:47:58 +0100

    Added handling of guesses with duplicate chars

--- Wordle.fsx
+++ Wordle.fsx
@@ -72,6 +72,8 @@ type Color =
     | Black
     | Yellow
     | Green
+    | BlackWithAnother
+    | YellowWithAnother
 
 let toColor =
     function
@@ -86,15 +88,56 @@ type Result = {
     Color: Color
 }
 
+let wordContainsCharTimes (c:char) (i:int) (word:string) : bool =
+    let chars = word |> Seq.filter (fun c1 -> c1 = c) 
+    Seq.length chars = i
+
 let filterWords (words:string[]) (result: Result) =
     match result.Color with
     | Yellow -> words |> Array.filter (fun word -> word.Contains(result.Char) && word[result.Position] <> result.Char)
     | Green -> words |> Array.filter (fun word -> word[result.Position] = result.Char)
     | Black -> words |> Array.filter (fun word -> word.Contains(result.Char) |> not)
+    | BlackWithAnother -> words |> Array.filter (fun word -> (word |> wordContainsCharTimes result.Char 1))
+    | YellowWithAnother -> words |> Array.filter (fun word -> word[result.Position] <> result.Char && word |> wordContainsCharTimes result.Char 2)
+
+let handleChar (resultsSoFar:Result list) (resultsForChar:Result list) = 
+    match resultsForChar with
+    | [] -> failwith "Ouch"
+    | [result] -> result :: resultsSoFar
+    | result1 :: result2 :: _ ->
+        match result1.Color, result2.Color with
+        | Green, Green
+        | Black, Black -> result1 :: result2 :: resultsSoFar
+        | Yellow, Yellow
+        | Yellow, Green -> {result1 with Color = YellowWithAnother} :: result2 :: resultsSoFar
+        | Green, Yellow -> result1 :: {result2 with Color = YellowWithAnother} :: resultsSoFar
+        | Black, Green
+        | Black, Yellow -> {result1 with Color = BlackWithAnother} :: result2 :: resultsSoFar
+        | Green, Black
+        | Yellow, Black -> result1 :: {result2 with Color = BlackWithAnother} :: resultsSoFar
+        | BlackWithAnother, _
+        | YellowWithAnother, _
+        | _, BlackWithAnother
+        | _, YellowWithAnother -> failwith "Boom"
+
+handleChar [] [{Char = 'r'; Position = 2; Color = Yellow};{Char = 'r'; Position = 3; Color = Green}]
+
+let handleDuplicateCharsInAnswer (answers:seq<Result>) =
+    let byChar = answers |> Seq.toList |> List.groupBy (fun r -> r.Char) |> List.map snd
+    byChar |> List.fold handleChar []
+
+handleDuplicateCharsInAnswer 
+    [
+        {Char = 'm'; Position = 0; Color = Black}
+        {Char = 'e'; Position = 1; Color = Green}
+        {Char = 'r'; Position = 2; Color = Green}
+        {Char = 'r'; Position = 3; Color = Black}
+        {Char = 'y'; Position = 3; Color = Green} ]
 
 let guess (word:string) (result:string) (words:string[]) =
     Seq.zip word result 
     |> Seq.mapi (fun pos (c, color) -> {Char = c; Position = pos; Color = color |> toColor })
+    |> handleDuplicateCharsInAnswer
     |> Seq.fold filterWords words
 
 //
@@ -123,9 +166,10 @@ let secondWord =
 
 let possibleWords =
     allWords
-    |> guess "arose" "BBYBB"
-    |> guess "unity" "YYBYB"
-    |> guess "count" "BGGGG"
+    |> guess "arose" "BYBBY"
+    |> guess "tired" "BBGYB"
+    |> guess "mercy" "BGGBG"
+    |> guess "jerky" "BGGGG"
 
 let withValues =
     possibleWords
